26c2bfddf4e24ee29805320de437e459d52a5ad3
Martin Fimia <martin.fimia@tui.com>
2024-04-03 18:15:22 +0200

    feat: fetch server data and display in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,33 @@
 import dynamic from 'next/dynamic'
-import { Fragment } from 'react'
+import { ComponentType, Fragment } from 'react'
 
-const Dashboard = dynamic(() => import("remote/dashboard"))
+interface IDashboardPageProps {
+  data: number[]
+}
 
-const DashboardPage = () => {
+const Dashboard: ComponentType<IDashboardPageProps> = dynamic(() => import("remote/dashboard"))
+
+const DashboardPage = ({ data }: IDashboardPageProps) => {
   return (
     <Fragment>
       <h1>Protected Page</h1>
-      <Dashboard />
+      <Dashboard data={data} />
     </Fragment>
   )
+}
+
+export async function getServerSideProps() {
+  // Fetch data from your API or other sources
+  const data = await fetchData();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const fetchData = async () => {
+  return [1, 2, 3]
 }
 
 export default DashboardPage
