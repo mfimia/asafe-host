6d43281b5f54a96e09caa5aec2de7ffa76020163
Martin Fimia <martin.fimia@tui.com>
2024-04-03 22:45:15 +0200

    chore: fetch data on remote component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,33 +1,14 @@
 import dynamic from 'next/dynamic'
-import { ComponentType, Fragment } from 'react'
+import { Fragment } from 'react'
 
-interface IDashboardPageProps {
-  data: number[]
-}
+const Dashboard = dynamic(() => import("remote/dashboard"))
 
-const Dashboard: ComponentType<IDashboardPageProps> = dynamic(() => import("remote/dashboard"))
-
-const DashboardPage = ({ data }: IDashboardPageProps) => {
+const DashboardPage = () => {
   return (
     <Fragment>
       <h1>Protected Page</h1>
-      <Dashboard data={data} />
+      <Dashboard />
     </Fragment>
   )
 }
-
-export async function getServerSideProps() {
-  // Fetch data from your API or other sources
-  const data = await fetchData();
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-const fetchData = async () => {
-  return [1, 2, 3]
-}
-
 export default DashboardPage
